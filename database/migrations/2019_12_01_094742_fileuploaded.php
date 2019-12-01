db522bc52b7dfab0f69fe267a7a3787df384fe7c
antonmartsyniuk <terr83@gmail.com>
2019-12-01 11:55:27 +0200

    up laravel 6+

---
+++ database/migrations/2019_12_01_094742_fileuploaded.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Fileuploaded extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_uploaded', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('department');
+            $table->text('type');
+            $table->text('category');
+            $table->text('brand');
+            $table->text('name');
+            $table->text('sku');
+            $table->longText('description');
+            $table->float('price', 10, 2);
+            $table->text('warranty');
+            $table->boolean('stock_status');
+            $table->timestamp('created')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_uploaded');
+    }
+}
